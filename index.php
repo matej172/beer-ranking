e3f495170e245cbc18e3b70bb9dd47dc33a61dc3
Matej Rábek <matej.rabek@gmail.com>
2021-02-22 23:06:22 +0100

    REMOVE: mysqli code

--- index.php
+++ index.php
@@ -16,30 +16,6 @@
     <?php
 
     // Connexion et sélection de la base
-    $conn = mysqli_connect('db', 'user', 'test', "myDb");
-
-
-    $query = 'SELECT * From Person';
-    $result = mysqli_query($conn, $query);
-
-    echo '<table class="table table-striped">';
-    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
-    while($value = $result->fetch_array(MYSQLI_ASSOC)){
-        echo '<tr>';
-        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
-        foreach($value as $element){
-            echo '<td>' . $element . '</td>';
-        }
-
-        echo '</tr>';
-    }
-    echo '</table>';
-
-    /* Libération du jeu de résultats */
-    $result->close();
-
-    mysqli_close($conn);
-
     ?>
     </div>
 </body>
