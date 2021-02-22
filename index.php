693fe1e583c2b7c7c2cfd8761bc8e5031659698e
Matej Rábek <matej.rabek@gmail.com>
2021-02-22 21:26:05 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<html>
+    <head lang="sk">
+	<meta charset="utf-8">
+	<title>Beer counter</title>
+	<meta name="author" content="Matej">
+    </head>
+    <body>
+	<h1>Beer counter</h1>
+    </body>
+</html>
